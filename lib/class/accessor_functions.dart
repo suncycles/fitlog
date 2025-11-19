8f39c57a526096c8025cb9f320826e792fc63513
Celsius273 <krp.gm2846297@protonmail.com>
2025-11-19 15:53:47 -0800

    Updated function for exercise history. Added several functions for accessing the database.

--- lib/class/accessor_functions.dart
+++ lib/class/accessor_functions.dart
@@ -2,25 +2,63 @@ import 'dart:async';
 import 'exercise_class.dart';
 import 'history_class.dart';
 import 'workout_class.dart';
+import 'dart:io';
 
-// All dummy functions. Replace with SQL queris
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 class WorkoutDatabase {
   static final WorkoutDatabase instance = WorkoutDatabase._init();
+  static Database? _database;
+
   WorkoutDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('workoutDB.db');
+    return _database!;
+  }
+
+
+  Future<Database> _initDB(String fileName) async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, fileName);
+
+    final exists = await File(path).exists();
+    if (!exists) {
+      final data = await rootBundle.load('DB/workoutDB.db'); 
+      final bytes = data.buffer.asUint8List();
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+
+    return await openDatabase(path, version: 1);
+  }
+
+  Future<Exercise?> getExercise(int id) async {
+    final db = await WorkoutDatabase.instance.database;
+
+    // Query the exercise_list table
+    final result = await db.query(
+      'exercise_list',
+      where: 'exercise_id = ?',
+      whereArgs: [id],
+    );
+
+
+    if (result.isEmpty) {
+      return null;
+    }
+
+    final exercise = Exercise.fromJson(result.first);
+
+    return exercise;
+  }
 
   Future<List<Exercise>> getExercises({String? primaryMuscle, String? search}) async {
     // Fetching exercises filtered by primaryMuscle
     return [];
-  }
-
-  Future<Exercise?> getExercise(int id) async {
-    // Fetching exercise with id
-    return null;
-  }
-
-  Future<int> createExercise(Exercise exercise) async {
-    // Inserting new exercise into db
-    return 1;
   }
 
   Future<List<String>> getPrimaryMuscles() async {
@@ -28,52 +66,83 @@ class WorkoutDatabase {
     return ['Chest', 'Back', 'Legs'];
   }
 
-  Future<int> createWorkout(Workout workout) async {
-    // Creating workout
-    return 1;
+  Future<List<ExerciseHistory>> getExerciseHistory(int workoutId) async {
+    // Fetching history for workoutId - do
+    final db = await database;
+    final List<ExerciseHistory> history = await db.query('exercise_history', where: 'workout_id = ?', whereArgs: [workoutId]).then((maps) => maps.map((map) => ExerciseHistory.fromJson(map)).toList());
+    return history;
   }
+ 
 
   Future<List<Workout>> getWorkouts() async {
     // Fetching all workouts
-    return [];
+    final db = await database;
+    final List<Workout> all_workouts = await db.query('workouts').then((maps) => maps.map((map) => Workout.fromJson(map)).toList());
+
+    if (all_workouts.isNotEmpty) {
+      return all_workouts;
+    } else {
+      return [];
+    }
+
   }
 
   Future<Workout?> getWorkout(int id) async {
     // Fetching workout with id
-    return null;
+    final db = await database;
+  
+    final List<Workout?> workouts = await db.query('workouts', where: 'workout_id = ?', whereArgs: [id]).then((maps) => maps.map((map) => Workout.fromJson(map)).toList());
+
+    if (workouts.isNotEmpty) {
+      return workouts.first;
+    } else {
+      return null;
+    }
+
   }
 
   Future<int> updateWorkout(Workout workout) async {
     // Updating workout by ID
-    return 1;
+    final db = await database;
+    final int? id = workout.id;
+    final List<Workout?> workouts = await db.query('workouts', where: 'workout_id = ?', whereArgs: [id]).then((maps) => maps.map((map) => Workout.fromJson(map)).toList());
+
+    if (workouts.isNotEmpty) {
+      return await db.update('workouts', workout.toJson(), where: 'workout_id = ?', whereArgs: [workout.  id]);
+    } else {
+      return 0;
+    }
+  
   }
 
   Future<int> deleteWorkout(int id) async {
     // Deleting workout by ID
-    return 1;
+    final db = await database;
+  
+    final List<Workout?> workouts = await db.query('workouts', where: 'workout_id = ?', whereArgs: [id]).then((maps) => maps.map((map) => Workout.fromJson(map)).toList());
+
+    if (workouts.isNotEmpty) {
+      return await db.delete('workouts', where: 'workout_id = ?', whereArgs: [id]);
+    } else {
+      return 0;
+    }
   }
 
   Future<int> createExerciseHistory(ExerciseHistory history) async {
-    // Logging history for exercise
-    return 1;
+    // Logging history for exercise 
+    final db = await database;
+
+    if (history.id != null) {
+      return await db.insert('exercise_history', history.toJson());
+    } else {
+      return 0;
+    }
   }
 
-  Future<List<ExerciseHistory>> getExerciseHistory(int workoutId) async {
-    // Fetching history for workoutId
-    return [];
-  }
-
-  Future<int> updateExerciseHistory(ExerciseHistory history) async {
-    // Updating exercise history by ID
-    return 1;
-  }
-
-  Future<int> deleteExerciseHistory(int id) async {
-    // Deleting exercise history by ID
-    return 1;
-  }
-
-  Future close() async {
-    // Closing dummy database connection
+  
+  Future<void> close() async {
+    final db = _database;
+    if (db != null) await db.close();
+    _database = null;
   }
 }
